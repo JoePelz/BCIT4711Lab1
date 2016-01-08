f1bcfc41a9f5018eb769553338779e91c8872ff0
Joe Pelz <joe.pelz@gmail.com>
2016-01-07 16:49:00 -0800

    added gitignore, string test

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-echo 'Hi, my name is';
+$temp = 'Jim. ';
+echo 'Hi, my name is ';
+echo $temp;
+$temp = 'geek. ';
+echo "I am a ";
+echo $temp;
+$temp = 10;
+echo 'My level is ';
+echo $temp;
 ?>
