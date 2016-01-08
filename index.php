ead2669415bedbdc4c96df456b07328860197d79
Joe Pelz <joe.pelz@gmail.com>
2016-01-07 20:15:50 -0800

    added interactivity, some comments

--- index.php
+++ index.php
@@ -1,29 +1,91 @@
+<DOCTYPE html>
+<html lang="en-CA">
+<head>
+	<title>Tic Tac Toe</title>
+</head>
+<body>
 <?php
 
 $squares = $_GET['board'];
 $game = new Game($squares);
 
-$game->display();
+echo '<h1>Tic Tac Toe</h1>';
+echo '<h3>World Championship Simulator</h3>';
+echo "<h5>You are 'x'</h5>";
 
-if ($game->winner('x'))
+if ($game->is_empty())
+	echo 'You have the first move.';
+else if ($game->winner('x'))
 	echo 'You win. Lucky guesses!';
 else if ($game->winner('o'))
 	echo 'I win. Muahahaaa';
+else if ($game->pick_move() && $game->winner('o'))
+	echo 'I win. Muahahahaha';
+else if ($game->no_moves()) 
+	echo "Game over. It's a tie this time....";
 else
-	echo 'No winner yet, but you are losing!';
+	echo 'So far so good. Your move, if you dare...';
 
 
+$game->display();
+
+echo '<br/>';
+echo '<a href="./?board=---------">New Game</a>';
+
+
+// 0 1 2
+// 3 4 5
+// 6 7 8
 class Game {
 	var $position;
 	var $newposition;
 
+	//Constructor for the game object.
+	//builds an array of positions out of the given string.
 	function __construct($squares) {
 		$this->position = str_split($squares);
 	}
 
+	//Check if the board is empty (i.e. a new-game state)
+	//returns true if the board is full of '-' only
+	function is_empty() {
+		for ($pos=0; $pos<9; $pos++) {
+			if ($this->position[$pos] <> '-')
+				return false;
+		}
+		return true;
+	}
+
+	//Check if the board is full (i.e. no moves possible)
+	//returns true if there are no '-' on the board.
+	function is_full() {
+		for ($pos=0; $pos<9; $pos++) {
+			if ($this->position[$pos] == '-')
+				return false;
+		}
+		return true;
+	}
+
+	//Places an 'o' on behalf of the computer.
+	//
+	//returns true if a token is placed. false otherwise.
+	function pick_move() {
+		//The middle square is the most vital to success!
+		if ($this->position[4] == '-') {
+			$this->position[$pos] = 'o';
+			return true;
+		}
+
+		for ($pos=0; $pos<9; $pos++) {
+			if ($this->position[$pos] == '-') {
+				$this->position[$pos] = 'o';
+				return true;
+			}
+		}
+		return false;
+	}
+
 	function display() {
-		echo '<h1>Tic Tac Toe</h1>';
-		echo '<h3>World Championship Simulator</h3>';
 		echo '<table cols="3" style="font-size:large; font-weight:bold">';
 		echo '<tr>';
 		for ($pos=0; $pos<9; $pos++) {
@@ -42,7 +104,7 @@ class Game {
 			return '<td>'.$token.'</td>';
 
 		$this->newposition = $this->position;
-		$this->newposition[$n] = 'o';
+		$this->newposition[$n] = 'x';
 		$move = implode($this->newposition);
 		$link = './?board='.$move;
 		return '<td><a href="'.$link.'">-</a></td>';
@@ -97,3 +159,5 @@ class Game {
 // 6 7 8
 
 ?>
+</body>
+</html>
