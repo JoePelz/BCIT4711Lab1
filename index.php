c425ee35c322cc3c412620f0993734d75c16d20d
Joe Pelz <joe.pelz@gmail.com>
2016-01-07 16:58:29 -0800

    ternary operator!

--- index.php
+++ index.php
@@ -2,5 +2,15 @@
 $name = 'Joe';
 $what = 'CST student';
 $level = 4;
-echo 'Hi, my name is '.$name.' and I am a level '.$level.' '.$what.'.'
+echo 'Hi, my name is '.$name.' and I am a level '.$level.' '.$what.'.';
+
+$hoursworked = 10;
+$rate = 12;
+if ($hoursworked > 40) {
+	$total = $hoursworked * $rate * 1.5;
+} else {
+	$total = $hoursworked * $rate;
+}
+echo "<br />";
+echo ($total > 0) ? 'You owe me '.$total : "You're welcome";
 ?>
