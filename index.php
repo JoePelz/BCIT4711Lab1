e4d0599831c7ec27ed5ee7b90440f5a4e8e4d03c
Joe Pelz <joe.pelz@gmail.com>
2016-01-07 20:54:16 -0800

    improved AI.  added comments. Prettified.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en-CA">
 <head>
 	<title>Tic Tac Toe</title>
+	<link rel="stylesheet" href="TTT.css">
 </head>
 <body>
 <?php
@@ -11,7 +12,7 @@ $game = new Game($squares);
 
 echo '<h1>Tic Tac Toe</h1>';
 echo '<h3>World Championship Simulator</h3>';
-echo "<h5>You are 'x'</h5>";
+echo "<h4>You are 'x'</h4>";
 
 if ($game->is_empty())
 	echo 'You have the first move.';
@@ -21,7 +22,7 @@ else if ($game->winner('o'))
 	echo 'I win. Muahahaaa';
 else if ($game->pick_move() && $game->winner('o'))
 	echo 'I win. Muahahahaha';
-else if ($game->no_moves()) 
+else if ($game->is_full()) 
 	echo "Game over. It's a tie this time....";
 else
 	echo 'So far so good. Your move, if you dare...';
@@ -70,13 +71,44 @@ class Game {
 	//
 	//returns true if a token is placed. false otherwise.
 	function pick_move() {
+		$corners = array(0, 2, 6, 8);
+
 		//The middle square is the most vital to success!
 		if ($this->position[4] == '-') {
-			$this->position[$pos] = 'o';
+			$this->position[4] = 'o';
 			return true;
 		}
 
+		//If an pos would cause victory, grab it.
+		//If a pos could cause defeat, steal it.
+		//Otherwise hold off for a corners.
 		for ($pos=0; $pos<9; $pos++) {
+			if ($this->position[$pos] == '-') {
+				$this->position[$pos] = 'o';
+				if ($this->winner('o')) {
+					return true;
+				} else {
+					$this->position[$pos] = 'x';
+					if ($this->winner('x')) {
+						$this->position[$pos] = 'o';
+						return true;
+					} else {
+						$this->position[$pos] = '-';
+					}
+				}
+			}
+		}
+
+		//Check corners. If they're free, take them.
+		foreach ($corners as $pos) {
+			if ($this->position[$pos] == '-') {
+				$this->position[$pos] = 'o';
+				return true;
+			}
+		}
+		
+		//Check edges again and take them if they're free.
+		for ($pos=1; $pos<9; $pos += 2) {
 			if ($this->position[$pos] == '-') {
 				$this->position[$pos] = 'o';
 				return true;
@@ -85,8 +117,10 @@ class Game {
 		return false;
 	}
 
+	//Echo the game board as a table
+	//to display
 	function display() {
-		echo '<table cols="3" style="font-size:large; font-weight:bold">';
+		echo '<table cols="3">';
 		echo '<tr>';
 		for ($pos=0; $pos<9; $pos++) {
 			echo $this->show_cell($pos);
@@ -97,11 +131,16 @@ class Game {
 		echo '</table>';
 	}
 
+	//Echo the contents of each table cell, 
+	//  as a <td> element.
+	//If a position is a viable play, 
+	//	echo it as an anchor to making that play.
 	function show_cell($n) {
 		$token = $this->position[$n];
 
-		if ($token <> '-') 
-			return '<td>'.$token.'</td>';
+		//TODO: make game state a Game property. track gameover.
+		if ($token <> '-' || $this->winner('o') || $this->winner('x')) 
+			return '<td class="'.$token.'">'.$token.'</td>';
 
 		$this->newposition = $this->position;
 		$this->newposition[$n] = 'x';
@@ -110,6 +149,8 @@ class Game {
 		return '<td><a href="'.$link.'">-</a></td>';
 	}
 
+	//Determine if the given token occurs in as three-in-a-row
+	// on the 3x3 game board.
 	function winner($token) {
 		$result = true;
 
@@ -151,12 +192,6 @@ class Game {
 		return $result;
 	}
 }
-
-
-//Game Board (indices):
-// 0 1 2
-// 3 4 5
-// 6 7 8
 
 ?>
 </body>
